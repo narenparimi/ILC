27fab2b03130cdeb0cb94d0d346bc73568fe669f
gharshain <gharsha.inbox@gmail.com>
2015-03-16 19:51:41 +0530

    Updating UI and style issues
    1. converted the breadcrumb component to directive.
    2. updated the styles of breadcrumb component.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,31 @@
-    angular.module('formBuilderApp',['ngRoute']).config(function($routeProvider){
-    //'use strict';
-    $routeProvider.when('/home',{
+    angular.module('formBuilderApp', ['ngRoute']).config(function ($routeProvider) {
+        //'use strict';
+        $routeProvider.when('/home', {
             templateUrl: 'views/home.html',
-            controllerAs: 'WizardsController'// map js to html scope
-    }).when('/step1',{
+            controllerAs: 'WizardsController' // map js to html scope
+        }).when('/step1', {
             templateUrl: 'views/step1.html',
-            controllerAs: 'WizardsController' 
-        }).when('/step2',{
+            controllerAs: 'WizardsController'
+        }).when('/step2', {
             templateUrl: 'views/step2.html',
-            controllerAs: 'WizardsController' 
-        }).when('/step3',{
+            controllerAs: 'WizardsController'
+        }).when('/step3', {
             templateUrl: 'views/step3.html',
-            controllerAs: 'WizardsController' 
-        }).when('/step4',{
+            controllerAs: 'WizardsController'
+        }).when('/step4', {
             templateUrl: 'views/step4.html',
-            controllerAs: 'WizardsController' 
-        }).when('/step5',{
+            controllerAs: 'WizardsController'
+        }).when('/step5', {
             templateUrl: 'views/step5.html',
-            controllerAs: 'WizardsController' 
-        }).when('/step6',{
+            controllerAs: 'WizardsController'
+        }).when('/step6', {
             templateUrl: 'views/final.html',
-            controllerAs: 'WizardsController' 
+            controllerAs: 'WizardsController'
         }).otherwise({
-            redirectTo : '/step1'
+            redirectTo: '/step1'
         });
-});    
+    }).directive('breadCrumb', function () {
+        return {
+            templateUrl: 'views/breadCrumbTemplate.html'
+        };
+    });
--- app/scripts/controllers/WizardController.js
+++ app/scripts/controllers/WizardController.js
@@ -1,31 +1,63 @@
-
+//TODO: Break this controller into tiny controllers.
 angular.module('formBuilderApp')
-  .controller('WizardsController',
-    function WizardsController( $scope,$location ) {
-      'use strict';
-	   $scope.model = {};
-	   
-       $scope.model.step1 = {};
-	   
-	   $scope.step2Model = {addAQuestion:'',na:false,naCamera:false,yn:false,ynCamera:false,desc:'',
-							staffLocation:'',flag:false,req:false};
-       $scope.model.step2 = [];
-       
-	   $scope.model.step3 = {};
-       $scope.model.step4 = {};
-       $scope.model.step5 = {};
-       $scope.model.step6 = {};
-	
-		$scope.createSurveyName = function(){
-			$scope.model.step1 = {formName:$scope.formName,formDescription:$scope.formDescription};
-			$scope.step2Model = {};
-			$location.path('/step2');
-		};
-	
-		$scope.addQuestion = function(){
-			//$scope.model.step2 = {na:$scope.na,yn:$scope.yn,desc:$scope.desc,staffLocation:$scope.staffLocation,flag:$scope.flag};
-			$scope.model.step2.push($scope.step2Model);
-			
-		};
-		
-    });
+    .controller('WizardsController',
+        function WizardsController($scope, $location) {
+            'use strict';
+            $scope.model = {};
+
+            $scope.model.step1 = {};
+
+            $scope.breadCrumbs = [{
+                id: '1',
+                value: 'Step 1'
+            }, {
+                id: '2',
+                value: 'Step 2'
+            }, {
+                id: '3',
+                value: 'Step 3'
+            }, {
+                id: '4',
+                value: 'Step 4'
+            }, {
+                id: '5',
+                value: 'Step 5'
+            }, {
+                id: '6',
+                value: 'Step 6'
+            }];
+
+            $scope.step2Model = {
+                addAQuestion: '',
+                na: false,
+                naCamera: false,
+                yn: false,
+                ynCamera: false,
+                desc: '',
+                staffLocation: '',
+                flag: false,
+                req: false
+            };
+            $scope.model.step2 = [];
+
+            $scope.model.step3 = {};
+            $scope.model.step4 = {};
+            $scope.model.step5 = {};
+            $scope.model.step6 = {};
+
+            $scope.createSurveyName = function () {
+                $scope.model.step1 = {
+                    formName: $scope.formName,
+                    formDescription: $scope.formDescription
+                };
+                $scope.step2Model = {};
+                $location.path('/step2');
+            };
+
+            $scope.addQuestion = function () {
+                //$scope.model.step2 = {na:$scope.na,yn:$scope.yn,desc:$scope.desc,staffLocation:$scope.staffLocation,flag:$scope.flag};
+                $scope.model.step2.push($scope.step2Model);
+
+            };
+
+        });
